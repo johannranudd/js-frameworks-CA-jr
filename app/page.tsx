b28b7fc81c7436ea4c35e7467241fce489e1e590
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-20 22:02:08 +0100

    working on tailwing configurations and dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { handleClick } from "./test";
+
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
-      <button className="bg-gray-500 rounded-md">Change color mode</button>
+      <h1 className="text-black dark:text-white">Home</h1>
+      <button onClick={handleClick} className="bg-gray-500 rounded-md">
+        Change color mode
+      </button>
     </main>
   );
 }
---
+++ app/test.ts
@@ -0,0 +1,12 @@
+"use client";
+export function handleClick() {
+  // const t = JSON.parse(localStorage.getItem("color-mode") || "");
+  // if (!t) {
+  //   console.log("no T");
+  // } else {
+  //   console.log("T");
+  //   console.log(t);
+  // }
+  console.log("handleClick");
+  //   change fore and background
+}
