c6a1c75b885627bc1d3bdbecc1843a7e4948be29
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-23 03:49:21 +0100

    working on prop drilling with typescript

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,49 @@ export default function Home() {
   const { userId, setUserId, data, setData } = useGlobalContext();
 
   useEffect(() => {
-    console.log(userId);
-    console.log(data);
-
-    setUserId("john doe");
-    setData([{ firstName: "tim" }, { firstName: "dim" }, { firstName: "lim" }]);
+    // console.log(userId);
+    // console.log(data);
   }, []);
+  // setUserId("john doe");
+  // setData([{ firstName: "tim" }, { firstName: "dim" }, { firstName: "lim" }]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (userId) {
+      setData((prev) => {
+        const newArray = [...prev, { firstName: userId }];
+        return newArray;
+      });
+    }
+  }
+
   return (
     <main>
       <h1>Home</h1>
       <BtnColorMode />
       <h3>in context</h3>
-      <ul>
-        {/* <li>{userId && userId}</li>
-        <li>{data ? data : []}</li> */}
-      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setUserId(e.target.value)}
+          className="border border-black dark:border-white"
+        />
+        <button type="submit">submit</button>
+      </form>
+      <UserNameList data={data} />
     </main>
   );
 }
+
+const UserNameList = (data: Object) => {
+  const { newData: any } = data;
+  console.log(newData);
+  return (
+    <ul>
+      {/* {data &&
+        data.map((user: Object) => {
+          <li>user.firstName</li>;
+        })} */}
+    </ul>
+  );
+};
