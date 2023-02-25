17fa8de88c14b9e8a9d3d29b77726c76bec3cfd0
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-25 16:02:47 +0100

    adds typed routes

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Link from "next/link";
 
 export default function NavBar() {
+  // comment
   return (
     <header className="border border-2 border-pink-400 space-x-3">
       <Link href={"/"}>Home</Link>
