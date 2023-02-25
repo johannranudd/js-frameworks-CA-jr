38c921740ec921238e45175f7f4500e3b836ce8d
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-25 20:13:25 +0100

    adds basePath and passHref

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -4,7 +4,9 @@ export default function NavBar() {
   return (
     <header>
       <nav className="border border-2 border-pink-400 space-x-3">
-        <Link href={"/"}>Home</Link>
+        <Link href={"/"} passHref>
+          Home
+        </Link>
         <Link href={"/products"}>Products</Link>
         <Link href={"/about"}>About</Link>
       </nav>
