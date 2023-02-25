a4fb97eb71d86fbcb41b83eeb2f0b05d148e6505
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-25 17:40:01 +0100

    removing btnComponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import BtnColorMode from "../components/ui/btnColorMode";
+// import BtnColorMode from "../components/ui/btnColorMode";
 import ListComponent from "../components/lists/listComponent";
 
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-      <BtnColorMode />
+      {/* <BtnColorMode /> */}
       <ListComponent />
     </main>
   );
