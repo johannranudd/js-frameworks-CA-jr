a6b86b0d76f7120a3832cff52035c4717d46523c
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-25 15:59:58 +0100

    C:/Program Files/Git/

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,8 +6,8 @@ export default function NavBar() {
   return (
     <header className="border border-2 border-pink-400 space-x-3">
       <Link href={"/"}>Home</Link>
-      <Link href={"./products"}>Products</Link>
-      <Link href={"./about"}>About</Link>
+      <Link href={"/products"}>Products</Link>
+      <Link href={"/about"}>About</Link>
     </header>
   );
 }
