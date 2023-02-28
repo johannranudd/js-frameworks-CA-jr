c1618c510ba6da2f389478b5d6f12e65368ba754
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-28 22:02:52 +0100

    adds footer and ready for starting fetch

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Head from "./head";
 import ColorThemeProvider from "../context/colorThemeProvider";
 import { ContextProvider } from "../context/context";
 import Navbar from "./components/navigation/Navbar";
+import Footer from "./components/navigation/Footer";
 
 export default function RootLayout({
   children,
@@ -12,11 +13,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head />
-      <body>
+      <body className="relative min-h-screen">
         <ContextProvider>
           <ColorThemeProvider>
             <Navbar />
             {children}
+            <Footer />
           </ColorThemeProvider>
         </ContextProvider>
       </body>
