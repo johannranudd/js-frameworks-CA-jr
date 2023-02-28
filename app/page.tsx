dd79c0705f982b35805b8a172cb5365940d21fa2
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-28 23:13:11 +0100

    fetching started

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import ListOfProducts from "./components/lists/ListOfProducts";
+
 export default function Home() {
   return (
     <main className="mb-20">
-      <h1>Home Page</h1>
+      {/* <h1>Home Page</h1> */}
+      <ListOfProducts />
     </main>
   );
 }
