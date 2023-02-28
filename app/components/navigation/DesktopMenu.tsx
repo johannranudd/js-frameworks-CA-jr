d3e4f337e0a25f13726d9f99bb605ba9a3ba1a14
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-02-28 19:13:00 +0100

    new build

--- app/components/navigation/DesktopMenu.tsx
+++ app/components/navigation/DesktopMenu.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { useGlobalContext } from "@/context/context";
 export default function DesktopMenu() {
-  //   const { menuIsOpen } = useGlobalContext();
   return (
     <div className="hidden md:inline space-x-2 ml-auto pr-4">
       <Link href={"/"}>Home</Link>
